8d2d0f9a35e202a680520e1a67d309c271402d4d
Marco cadete <marco.cadete@gmail.com>
2021-02-11 00:42:17 +0100

    Added a form and more styling to the Header container.

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -8,11 +8,16 @@ function Header() {
                 <img src={berty} alt="mascot"/>
             </div>
             <div className="tu-header__intro">
-                <h1>Welcome to teenyURL!</h1>
+                <h2>Welcome to</h2>
+                <h1>teenyURL!</h1>
                 <p>Consectetur dolor nobis tempora expedita officiis exercitati
                 onem Accusamus officiis quo eaque eum non Corporis ea repellend
                 us reprehenderit voluptate illum repellendus aliquam Expedita a
                 ccusantium quaerat placeat ipsam quae! Ut sunt reiciendis.</p>
+                <form className="tu-header__form">
+                    <label for="url_input">Enter a long URL to make it a teenyURL</label>
+                    <input type="text" id="url_input" />
+                </form>
             </div>
         </div>
     );
