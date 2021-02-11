27a0826100e97b3438cac60708675baf11dfe6e1
Marco cadete <marco.cadete@gmail.com>
2021-02-11 15:21:08 +0100

    Added a components folder and created a Button component. Also added some CSS classes for margin spacing and font sizes.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,34 @@
+import "./Button.css";
+
+function Button(props) {
+    // Defaults
+    let classes = ["tu-btn", "tu-btn--green"];
+    let type = "button";
+    let value = props.value ? props.value : "";
+    let disabled = props.disabled ? true : false;
+
+    // Options
+    let colors = ["green", "yellow", "blue", "brown"];
+    let types = ["button", "submit", "reset"];
+
+    if (props.color && colors.includes(props.color)) {
+        classes[1] = `tu-btn--${props.color}`;
+    }
+
+    if (props.type && types.includes(props.type)) {
+        type = props.type;
+    }
+
+    return (
+        <button
+            className={classes.join(" ")}
+            type={type}
+            value={value}
+            disabled={disabled}
+        >
+            {props.children}
+        </button>
+    );
+}
+
+export default Button;
--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,22 +1,29 @@
 import "./Header.css";
-import berty from "../../images/berty.png"
+import berty from "../../images/berty.png";
+import Button from "../../components/Button/Button";
 
 function Header() {
     return (
         <div className="tu-header">
             <div className="tu-header__image">
-                <img src={berty} alt="mascot"/>
+                <img src={berty} alt="mascot" />
             </div>
             <div className="tu-header__intro">
                 <h2>Welcome to</h2>
                 <h1>teenyURL!</h1>
-                <p>Consectetur dolor nobis tempora expedita officiis exercitati
-                onem Accusamus officiis quo eaque eum non Corporis ea repellend
-                us reprehenderit voluptate illum repellendus aliquam Expedita a
-                ccusantium quaerat placeat ipsam quae! Ut sunt reiciendis.</p>
+                <p>
+                    Consectetur dolor nobis tempora expedita officiis exercitati
+                    onem Accusamus officiis quo eaque eum non Corporis ea
+                    repellend us reprehenderit voluptate illum repellendus
+                    aliquam Expedita a ccusantium quaerat placeat ipsam quae! Ut
+                    sunt reiciendis.
+                </p>
                 <form className="tu-header__form">
-                    <label for="url_input">Enter a long URL to make it a teenyURL</label>
+                    <label for="url_input">
+                        Enter a long URL to make it a teenyURL
+                    </label>
                     <input type="text" id="url_input" />
+                    <Button>Create</Button>
                 </form>
             </div>
         </div>
