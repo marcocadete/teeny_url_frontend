76e84d9fd2d6ec2bc81c34586412546190a051e6
Marco cadete <marco.cadete@gmail.com>
2021-02-15 20:22:44 +0100

    Completed the footer container.

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,11 +1,25 @@
 import "./Footer.css";
+import githubLogo from "../../images/github.png";
+// Components
+import Heading from "../../components/Heading/Heading";
 
 function Footer() {
     return (
         <footer className="tu-footer">
+            <Heading title="Contact" color="dark-yellow" />
+            <div className="tu-footer__content">
+                <div className="tu-footer__contact">
+                    <a href="https://github.com/marcocadete">
+                        <img src={githubLogo} />
+                        Get in touch with the developer on Github
+                    </a>
+                </div>
+                <div className="tu-footer__copyright">
+                    &copy; Copyright teenyURL 2021. All rights reserved.
+                </div>
+            </div>
         </footer>
     );
-
 }
 
 export default Footer;
